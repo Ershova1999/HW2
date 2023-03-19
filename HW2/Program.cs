c260f64cf98502bf7418ba3859837f9a849e33a7
Anastasia <Anastasia@LAPTOP-SKHANDQ9>
2023-03-19 17:02:20 +0300

    Добавьте файлы проекта.

---
+++ HW2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+public class Program
+{
+    public static void Main(String[] args)
+    {
+        int[] v = { 1, 5, 5, 2, 4 };
+
+        SortedSet<int> s = new SortedSet<int>();
+
+        for (int i = 0; i < v.Length; i++)
+            s.Add(v[i]);
+
+        List<int> ans = new List<int>();
+
+        foreach (int res in s)
+        {
+            ans.Add(res);
+        }
+
+        int n = ans.Count;
+
+        Console.Write("Второй наибольший элемент:");
+        Console.Write(ans[n - 2]);
+    }
+}
